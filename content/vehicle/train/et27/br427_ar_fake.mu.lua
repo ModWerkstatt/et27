17ebdda8cd3b555030fa86cb44a64fa0cd2e0100
Florian Thiel <flth@flth.de>
2026-06-14 01:06:18 +0200

    Anpassung an Build 40251

---
+++ content/vehicle/train/et27/br427_ar_fake.mu.lua
@@ -0,0 +1,13 @@
+﻿function data()
+return {
+	vehicles = {
+		{ name = "br427.1_ar_fake.mdl", forward = true },
+      { name = "br827_ar_fake.mdl", forward = true },
+      { name = "br427.4_ar_fake.mdl", forward = false },
+	},
+	name = _("fake") ..  _("br427_ar_name"),
+	desc = _("br427_ar_desc"),
+   groupFileName = "menu_et27.mdl",
+   filterTags = {"default"}
+}
+end
---
+++ content/vehicle/train/et27/br427_ob.mu.lua
@@ -0,0 +1,13 @@
+﻿function data()
+return {
+	vehicles = {
+		{ name = "br427.1_ob.mdl", forward = true },
+      { name = "br827_ob.mdl", forward = true },
+      { name = "br427.4_ob.mdl", forward = false },
+	},
+	name = _("br427_ob_name"),
+	desc = _("br427_ob_desc"),
+   groupFileName = "menu_et27.mdl",
+   filterTags = {"default"}
+}
+end
